feea43f0e90b10500938cfbb16f7e7b8ab806c51
Henrique Rodrigues <henryqrm@gmail.com>
2019-03-07 13:45:11 -0300

    update git sync

--- git-sync.sh
+++ git-sync.sh
@@ -2,7 +2,7 @@
 
 echo "Syncing..."
 git fetch > /dev/null
-git pull > /dev/null
+git pull origin master > /dev/null
 
 if [ `git status -s -u | grep -c -e "^??\|M\|D"` -gt 0 ]; then
     git add --all . > /dev/null
